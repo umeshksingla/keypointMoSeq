fix log base for information calculation

keypoint_moseq/analysis/bits.py
@@ -7,27 +7,9 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import multivariate_normal
 
 from keypoint_moseq.project.io import load_checkpoint, save_llh
-from keypoint_moseq.project.fit_utils import calculate_ll
-
-
-def fit_mvn(data):
-    """
-    Multivariate gaussian model for the pose prediction to get the lower bound
-    """
-
-    n_features = data['Y'].shape[-2]
-    d = data['Y'].shape[-1]
-
-    x = data['Y'][data['mask'] > 0]
-    x = x.reshape((-1, n_features * d))
-
-    p = multivariate_normal(mean=np.mean(x, axis=0), cov=np.cov(x.T)).pdf(x)
-    p = np.maximum(p, 1e-100)
-    log_Y_given_mvn = np.sum(np.log(p))
-    return log_Y_given_mvn
+from keypoint_moseq.project.fit_utils import calculate_ll, fit_mvn
 
 
 def get_logll_from_checkpoint(ckp):
@@ -79,7 +61,7 @@ def process_checkpoints(project_dir):
                     b = ckp['current_batch']
                     b_log_Y_and_model, b_log_Y_given_model, b_n_samples, b_log_Y_given_mvn = get_logll_from_checkpoint(ckp)
                     b_n_iters = list(ckp['history'].keys())
-                    b_bits = ((np.array(b_log_Y_given_model) - b_log_Y_given_mvn) / b_n_samples).tolist()
+                    b_bits = np.log2(np.exp((np.array(b_log_Y_given_model) - b_log_Y_given_mvn) / b_n_samples)).tolist()
 
                     log_Y_and_model.append(b_log_Y_and_model)
                     log_Y_given_model.append(b_log_Y_given_model)

keypoint_moseq/run/hyperparams/get_test_probs.py
@@ -14,10 +14,11 @@ parameters from the results.h5, find the model likelihood of the data
 and 
 
 """
-
+import numpy as np
 from jax_moseq.models import keypoint_slds
 import keypoint_moseq as kpm
-from keypoint_moseq.project.fit_utils import find_sleap_paths, calculate_ll
+from keypoint_moseq.project.fit_utils import find_sleap_paths, calculate_ll, fit_mvn
+from keypoint_moseq.project.io import save_llh
 import os
 import argparse
 from rich.pretty import pprint
@@ -94,7 +95,21 @@ def get_test_probs(test_paths, checkpoint_path, project_dir, save_dir, use_insta
     # Compute log likelihoods
     log_Y_and_model, log_Y_given_model = calculate_ll(model['states'], model['params'],
                                                       model['hypparams'], model['noise_prior'], data)
-    return log_Y_and_model, log_Y_given_model
+    log_Y_given_mvn = fit_mvn(data)
+    n_samples = np.sum(data['mask']).astype(int)
+    nats = ((log_Y_given_model - log_Y_given_mvn) / n_samples)
+    bits = np.log2(np.exp(nats))
+
+    test_llh = {
+        'log_Y_and_model': log_Y_and_model,
+        'log_Y_given_model': log_Y_given_model,
+        'log_Y_given_mvn': log_Y_given_mvn,
+        'n_samples': n_samples,
+        'bits': bits
+    }
+
+    save_llh(llh_path=os.path.join(save_dir, 'test_llh.p'))
+    return bits
 
 
 if __name__ == "__main__":